1c58bb68240274465b00c0876d3462241ae2be3f
Filip Lejhanec <flejhanec@seznam.cz>
2017-11-04 17:58:08 +0000

    Added NavBar

--- snails-fe/src/containers/Home.js
+++ snails-fe/src/containers/Home.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import ChatBox from '../components/ChatBox';
+import NavBar from '../components/NavBar';
 
 /*
   Entry point of the app
@@ -13,6 +14,7 @@ class Home extends Component {
       <div>
         <h1>Home</h1>
         <ChatBox />
+        <NavBar />
       </div>
     );
   }
