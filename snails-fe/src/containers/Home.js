dc279b76ed301821340d686ec5b6a5b9d1413030
Eva Topuzova <topuzovaeva@gmail.com>
2017-11-04 22:23:32 +0000

    asdf

--- snails-fe/src/containers/Home.js
+++ snails-fe/src/containers/Home.js
@@ -15,19 +15,31 @@ class Home extends Component {
     return (
       <div>
         <NavBar />
-        <h1>Welcome!</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <img src={logo} className='img-responsive' />
+        <h1 className='text-center'>Welcome!</h1>
         <Grid>
           <Row>
+            <Col xs={12} md={4} className='text-center'>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </Col>
+            <Col xs={12} md={4}>
+                <img src={logo} className='img-responsive' />
+            </Col>
+            <Col xs={12} md={4} className='text-center'>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </Col>
+          </Row>
+          <Row>
             <Col xs={12} md={6} className='text-center'>
-              <Button bsStyle='info' bsSize='large'>User</Button>
+              <Button bsStyle='info' bsSize='large' block>User</Button>
             </Col>
             <Col xs={12} md={6} className='text-center'>
-              <Button bsStyle='warning' bsSize='large'>Responder</Button>
+              <Button bsStyle='warning' bsSize='large' block>Responder</Button>
             </Col>
           </Row>
         </Grid>
