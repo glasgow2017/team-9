a3311b63ca4d68a3f8c374cef8ea8b623fcef28d
Eva Topuzova <topuzovaeva@gmail.com>
2017-11-04 19:15:54 +0000

    Login

---
+++ api/DB.php
@@ -0,0 +1,11 @@
+<?php
+        $host = 'localhost';
+        //devweb2017.cis.strath.ac.uk
+        $user = 'root';
+        //ptb15137
+        $pass = '';
+        //butike7haTho
+        $db = 'people';
+        $conn = mysqli_connect($host,$user,$pass,$db);
+
+
